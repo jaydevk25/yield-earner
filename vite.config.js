577dc2c1af5649dcdf77e76d61d9fda2048d3940
jaydevk25 <jaydevkumarjk25@gmail.com>
2025-10-21 09:29:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',       // ensures Vite uses project root
   build: {
     outDir: 'dist',
   },
